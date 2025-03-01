c39378c466f5a9f67dee4a816abdfa998d054cad
Trent Zeng <trentzz.dev@gmail.com>
2025-03-01 15:25:51 +1100

    Revert local edits for setup.py and _version for testing

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mity",
+    name="{NAME}",
     version=version,
     description="A sensitive Mitochondrial variant detection pipeline from WGS data",
     long_description=long_description,
